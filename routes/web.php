938ce1768d9e01d6eafb98efc231660b25993543
yi chu hsu <b28203598@yahoo.com.tw>
2022-12-12 20:04:30 +0800

    設定跳轉路由

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'welcome';
 });
-
+Route::get('r2', function() {
+    return view('welcome');
+});
+Route::get('r1', function() {
+    return redirect('r2');
+});
