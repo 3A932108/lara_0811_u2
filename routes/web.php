6263c29446efcae7991f50451b084211e6a0d0eb
yi chu hsu <b28203598@yahoo.com.tw>
2022-12-12 20:02:38 +0800

    設定回傳字串

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'welcome';
 });
-Route::get('/', function () {
-    return view('welcome');
-});
+
